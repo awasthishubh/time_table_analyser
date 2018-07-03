7548549843f60f786ecbdb11e42cc6411b47bea5
Shubham Awasthi <aw.shubh@gmail.com>
2018-07-03 12:43:03 +0530

    Used specific color code instead of range for better prediction

--- src/preproc_beta.py
+++ src/preproc_beta.py
@@ -11,17 +11,12 @@ r = 190
 for i in range(d[0]):
 	for j in range(d[1]):
 		p = im.getpixel((i,j))
-		if p[0] <= 200 and p[1] <= 200 and p[2] <= 200: #preprocessing 1
-			out.putpixel((i,j), (0,0,0))
-		elif p[1] > 200 and p[2] > 200 and p[2] >= p[1]: #preprocessing 2
-			out.putpixel((i,j), (0,0,200))
+		if ((p[0] == 255 and p[1] == 255 and p[2] == 204) or (p[0] == 249 and p[1] == 239 and p[2] == 164)) : #preprocessing 1
+			out.putpixel((i,j), (255,255,0))
+		elif p[0] == 204 and p[1] == 255 and p[2] == 51: #preprocessing 2
+			out.putpixel((i,j), (0,255,0))
 		else: #preprocessing 3 (monotonizing 2)
-			dg = dist((203,254,51), p)
-			dy = dist((255,255,204), p)
-			if dg < dy:
-				out.putpixel((i,j), (0,255,0))
-			else:
-				out.putpixel((i,j), (255,255,0))
+			out.putpixel((i,j), (0,0,0))
 
 
 out.rotate(180).save("ttpxt.png")
